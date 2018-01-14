d7f599ef404994d6c761a0903277743672a8e318
PocketNinjaCoUk <eyeamaman@gmail.com>
2018-01-13 23:06:18 -0800

    test with a few peekaboo items

--- src/pages/Holding.js
+++ src/pages/Holding.js
@@ -12,6 +12,16 @@ class PageHolding {
       $element: $('<div class="eggsa"></div>'),
     });
     this.mainNinja.init();
+
+    this.test1 = new Peekaboo({
+      $element: $('#test1'),
+    });
+    this.test1.init();
+
+    this.test2 = new Peekaboo({
+      $element: $('#test2'),
+    });
+    this.test2.init();
   }
 }
 
