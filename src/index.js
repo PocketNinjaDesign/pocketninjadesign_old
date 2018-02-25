e333b3665a327105bdca8e364b52211d78abcd99
PocketNinjaCoUk <eyeamaman@gmail.com>
2018-02-24 17:20:59 -0800

    Index page clean up

--- src/index.js
+++ src/index.js
@@ -1,34 +1,5 @@
 const css = require('../styles/primary.scss');
 
-// import $ from 'jQuery';
-// import globals from './globals';
-
-
-// Modules
-// import SideNavigation from './modules/nav/SideNavigation';
-
-
-// Services
-
-
-// Sections
-// import Portfolio from './modules/section/Portfolio';
-// import Contact from './modules/section/Contact';
-// import Codepen from './modules/section/Codepen';
-
-
-//
-// Index Script
-//
-// SideNavigation.init();
-
-// this will activate when scrolled on the page later on
-// Portfolio.init();
-// Codepen.init();
-// Contact.init();
-
-
-// Temp Holding Page
 import PageHolding from './pages/Holding';
 import PortfolioLanding from './pages/PortfolioLanding';
 
@@ -38,7 +9,3 @@ if ($('body').hasClass('page-holding')) {
 else if($('body').hasClass('page-portfolio-landing')) {
   PortfolioLanding.init();
 }
-
-// window.onpopstate = function(e) {
-//   console.log(JSON.stringify(e.state));
-// };
