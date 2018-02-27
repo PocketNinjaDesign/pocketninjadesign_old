e6139dd375a9272f220a59bb2ab145f215836a4f
PocketNinjaCoUk <eyeamaman@gmail.com>
2018-02-26 22:12:09 -0800

    Added some text for the categories.  Not the best but just look at the pretty pictures will you!!!

--- src/data/siteData.js
+++ src/data/siteData.js
@@ -7,21 +7,21 @@ module.exports = {
     title: 'UI Design',
     subTitle: 'WEB / CD-ROM',
     shortDescription: 'UI Design Short Description',
-    detailedDescription: 'UI Design Detailed Description',
+    detailedDescription: "I've created UI Designs since the start of my career working on CD-ROMs and Flash sites.  I then moved on to Web Design which initially was straight forward until the levels of complexity with Responsive Design moved in.  I now love the challenges that Responsive Design brings and feel some generic template boundaries needs pushing especially in transitioning between content which is a whole new ball game. All of the following pieces are Website, CD-ROM and Flash designs and mostly fairly bespoke.",
   }, {
     page: 'graphics.html',
     className: 'page-portfolio',
     title: 'Graphics',
     subTitle: 'Logos / Icons / Visuals',
     shortDescription: 'Graphics Short Description',
-    detailedDescription: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent fermentum nisl id nunc feugiat gravida. Phasellus laoreet sem quam, id placerat nibh accumsan vitae. Nunc sollicitudin posuere ultrices. Praesent fermentum dolor id nibh sodales aliquam. Morbi leo mauris, fringilla in arcu in, pharetra dictum ipsum. Nunc id risus et lectus suscipit efficitur at in tortor.',
+    detailedDescription: "From Branding, Illustration and Icons I've worked on a lot of graphics. From winning a pitch to make an e-card with Puma in my early days to knocking out logos in a morning and accepted by the client during lunch.  UI and Branding always does better with that little extra Oooomph otherwise it's just colored in wireframes right..? With a huge photo or video filling the screen on the home page right?",
   }, {
     page: 'illustration.html',
     className: 'page-portfolio',
     title: 'Illustration',
-    subTitle: 'Digital / Real',
-    shortDescription: 'Graphics Short Description',
-    detailedDescription: 'Graphics Detailed Description',
+    subTitle: 'Digital / Paper',
+    shortDescription: 'Blah blah blah in the car',
+    detailedDescription: "What's this? A front end design / developer who can draw? And not just with vectors making simple blocked shapes? Yes, I do like drawing, mostly black and white with a little red thrown in as you will see from the gallery.  I do have a penchant for zombies I won't lie to you.",
   }],
 
 
