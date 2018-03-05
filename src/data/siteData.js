b72e1c57dc59457d95657dafb679410d4e0f38f9
PocketNinjaCoUk <eyeamaman@gmail.com>
2018-03-05 00:43:11 -0800

    Added gallery images and made a start on organizing the sitedata

--- src/data/siteData.js
+++ src/data/siteData.js
@@ -1,28 +1,35 @@
 
 module.exports = {
 
-  navigation: [{
-    page: 'ui-design.html',
-    className: 'page-portfolio',
-    title: 'UI Design',
-    subTitle: 'WEB / CD-ROM',
-    shortDescription: 'UI Design Short Description',
-    detailedDescription: "I've created UI Designs since the start of my career working on CD-ROMs and Flash sites.  I then moved on to Web Design which initially was straight forward until the levels of complexity with Responsive Design moved in.  I now love the challenges that Responsive Design brings and feel some generic template boundaries needs pushing especially in transitioning between content which is a whole new ball game. All of the following pieces are Website, CD-ROM and Flash designs and mostly fairly bespoke.",
-  }, {
-    page: 'graphics.html',
-    className: 'page-portfolio',
-    title: 'Graphics',
-    subTitle: 'Logos / Icons / Visuals',
-    shortDescription: 'Graphics Short Description',
-    detailedDescription: "From Branding, Illustration and Icons I've worked on a lot of graphics. From winning a pitch to make an e-card with Puma in my early days to knocking out logos in a morning and accepted by the client during lunch.  UI and Branding always does better with that little extra Oooomph otherwise it's just colored in wireframes right..? With a huge photo or video filling the screen on the home page right?",
-  }, {
-    page: 'illustration.html',
-    className: 'page-portfolio',
-    title: 'Illustration',
-    subTitle: 'Digital / Paper',
-    shortDescription: 'Blah blah blah in the car',
-    detailedDescription: "What's this? A front end design / developer who can draw? And not just with vectors making simple blocked shapes? Yes, I do like drawing, mostly black and white with a little red thrown in as you will see from the gallery.  I do have a penchant for zombies I won't lie to you.",
-  }],
+  // navigation: require('./navigation'),
+  navigation: [
+    {
+      page: 'ui-design.html',
+      className: 'page-portfolio',
+      title: 'UI Design',
+      subTitle: 'WEB / CD-ROM',
+      shortDescription: 'UI Design Short Description',
+      detailedDescription: "I've created UI Designs since the start of my career working on CD-ROMs and Flash sites.  I then moved on to Web Design which initially was straight forward until the levels of complexity with Responsive Design moved in.  I now love the challenges that Responsive Design brings and feel some generic template boundaries needs pushing especially in transitioning between content which is a whole new ball game. All of the following pieces are Website, CD-ROM and Flash designs and mostly fairly bespoke."
+    },
+
+    {
+      page: 'graphics.html',
+      className: 'page-portfolio',
+      title: 'Graphics',
+      subTitle: 'Logos / Icons / Visuals',
+      shortDescription: 'Graphics Short Description',
+      detailedDescription: "From Branding, Illustration and Icons I've worked on a lot of graphics. From winning a pitch to make an e-card with Puma in my early days to knocking out logos in a morning and accepted by the client during lunch.  UI and Branding always does better with that little extra Oooomph otherwise it's just colored in wireframes right..? With a huge photo or video filling the screen on the home page right?"
+    },
+    
+    {
+      page: 'illustration.html',
+      className: 'page-portfolio',
+      title: 'Illustration',
+      subTitle: 'Digital / Paper',
+      shortDescription: 'Blah blah blah in the car',
+      detailedDescription: "What's this? A front end design / developer who can draw? And not just with vectors making simple blocked shapes? Yes, I do like drawing, mostly black and white with a little red thrown in as you will see from the gallery.  I do have a penchant for zombies I won't lie to you."
+    }
+  ],
 
 
 
@@ -34,137 +41,14 @@ module.exports = {
       thumb: 'images/portfolio/ui-design/thumb/',
       detail: 'images/portfolio/ui-design/detail/'
     },
-    items: [{
-      title: 'Personal Portfolio Site',
-      type: 'Web',
-      externalLink: {
-        url: 'https://xd.adobe.com/view/99c2179f-c05c-4afc-b97b-3ea51207c508?fullscreen',
-        text: 'View Prototype',
-      },
-      img: {
-        thumb: 'pocketninjadesign-adobe-xd-thumb-large.png',
-        detail: [{
-          bgColor: '#e4e4e4',
-          src: {
-            large: 'pocketninjadesign-adobe-xd-all-ui-large.png',
-            medium: 'pocketninjadesign-adobe-xd-all-ui-medium.png',
-            small: 'pocketninjadesign-adobe-xd-all-ui-small.png',
-          }
-        }, {
-          bgColor: '#e4e4e4',
-          src: {
-            large: 'pocketninjadesign-adobe-xd-desktop-large.png',
-            medium: 'pocketninjadesign-adobe-xd-desktop-medium.png',
-            small: 'pocketninjadesign-adobe-xd-desktop-small.png',
-          }
-        }, {
-          bgColor: '#e4e4e4',
-          src: {
-            large: 'pocketninjadesign-adobe-xd-mobile-large.png',
-            medium: 'pocketninjadesign-adobe-xd-mobile-medium.png',
-            small: 'pocketninjadesign-adobe-xd-mobile-small.png',
-          }
-        }],
-      }
-    },
-    
-    {
-      title: 'Blinkbox Tesco Hackathon',
-      type: 'Web',
-      img: {
-        thumb: 'Blinkbox-tesco-hackathon-ui-home-thumb-large.png',
-        detail: [{
-          bgColor: '#181818',
-          src: {
-            large: 'Blinkbox-tesco-hackathon-ui-home-detail-large.png',
-            medium: 'Blinkbox-tesco-hackathon-ui-home-detail-medium.png',
-            small: 'Blinkbox-tesco-hackathon-ui-home-detail-small.png',
-          }
-        }, {
-          bgColor: '#181818',
-          src: {
-            large: 'Blinkbox-tesco-hackathon-ui-explore-detail-large.jpg',
-            medium: 'Blinkbox-tesco-hackathon-ui-explore-detail-medium.jpg',
-            small: 'Blinkbox-tesco-hackathon-ui-explore-detail-small.jpg',
-          }
-        }, {
-          bgColor: '#181818',
-          src: {
-            large: 'Blinkbox-tesco-hackathon-ui-sub-category-detail-large.jpg',
-            medium: 'Blinkbox-tesco-hackathon-ui-sub-category-detail-medium.jpg',
-            small: 'Blinkbox-tesco-hackathon-ui-sub-category-detail-small.jpg',
-          }
-        }, {
-          bgColor: '#181818',
-          src: {
-            large: 'Blinkbox-tesco-hackathon-ui-player-detail-large.jpg',
-            medium: 'Blinkbox-tesco-hackathon-ui-player-detail-medium.jpg',
-            small: 'Blinkbox-tesco-hackathon-ui-player-detail-small.jpg',
-          }
-        }],
-      }
-    },
-    
-    {
-      title: 'Go Fishing Magazine',
-      type: 'Web',
-      img: {
-        thumb: 'go-fishing-magazine-website.jpg',
-        detail: [{
-          bgColor: '#000',
-          src: {
-            large: 'go-fishing-magazine-homepage.jpg',
-            medium: 'go-fishing-magazine-homepage.jpg',
-            small: 'go-fishing-magazine-homepage.jpg',
-          }
-        }, {
-          bgColor: '#000',
-          src: {
-            large: 'go-fishing-magazine-category.jpg',
-            medium: 'go-fishing-magazine-category.jpg',
-            small: 'go-fishing-magazine-category.jpg',
-          }
-        }, {
-          bgColor: '#000',
-          src: {
-            large: 'go-fishing-magazine-product.jpg',
-            medium: 'go-fishing-magazine-product.jpg',
-            small: 'go-fishing-magazine-product.jpg',
-          }
-        }],
-      }
-    },
-    
-    
-    {
-      title: 'Car Magazine',
-      type: 'Web',
-      img: {
-        thumb: 'car-magazine-website.jpg',
-        detail: [{
-          bgColor: '#0d0d0d',
-          src: {
-            large: 'car-magazine-homepage.jpg',
-            medium: 'car-magazine-homepage.jpg',
-            small: 'car-magazine-homepage.jpg',
-          }
-        }, {
-          bgColor: '#0d0d0d',
-          src: {
-            large: 'car-magazine-category.jpg',
-            medium: 'car-magazine-category.jpg',
-            small: 'car-magazine-category.jpg',
-          }
-        }, {
-          bgColor: '#0d0d0d',
-          src: {
-            large: 'car-magazine-detail.jpg',
-            medium: 'car-magazine-detail.jpg',
-            small: 'car-magazine-detail.jpg',
-          }
-        }],
-      }
-    }]
+
+    items: [
+      // require('./ui-design/pocketNinjaDesignWeb'),
+      // require('./ui-design/blinkboxHackathon'),
+      // require('./ui-design/blinkboxMusicWebsite'),
+      // require('./ui-design/goFishingMagazine'),
+      // require('./ui-design/carMagazine')
+    ]
   },
 
 
